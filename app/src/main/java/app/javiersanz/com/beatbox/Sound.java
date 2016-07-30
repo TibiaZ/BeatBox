c78811bf75c6d87d978372317768764a490d4697
Javier Sanz <javi95sanz@gmail.com>
2016-07-30 19:47:53 +0200

    List of sounds added to the interface!

---
+++ app/src/main/java/app/javiersanz/com/beatbox/Sound.java
@@ -0,0 +1,33 @@
+package app.javiersanz.com.beatbox;
+
+/**
+ * Created by TibiaZ on 30/07/2016.
+ * This class have an object responsible for keeping track of the filename,
+ * the name the user should see, and any other information related to that sound.
+ */
+public class Sound {
+
+    // Variables
+
+    private String mAssetPath;
+    private String mName;
+
+    // Constructor
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    // Getters and setters
+
+    public String getAssetPath(){
+        return mAssetPath;
+    }
+
+    public String getName(){
+        return mName;
+    }
+}
