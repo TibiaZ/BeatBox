722bb4bc52aed226d98d5c124a2d878405826342
Javier Sanz <javi95sanz@gmail.com>
2016-07-30 13:28:01 +0200

    Skeleton up and running!

---
+++ app/src/main/java/app/javiersanz/com/beatbox/BeatBoxFragment.java
@@ -0,0 +1,65 @@
+package app.javiersanz.com.beatbox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Tibiaz on 30/07/2016.
+ */
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance(){
+        return new BeatBoxFragment();
+    }
+
+    // onCreateView
+
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
+
+        RecyclerView recyclerView = (RecyclerView)view
+                .findViewById(R.id.fragment_beat_box_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        recyclerView.setAdapter(new SoundAdapter());
+
+        return view;
+    }
+
+    // SoundHolder
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+        private Button mButton;
+
+        public SoundHolder(LayoutInflater inflater, ViewGroup container){
+            super(inflater.inflate(R.layout.list_item_sound, container, false));
+
+            mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
+        }
+    }
+
+    // SoundAdapter
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new SoundHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder soundHolder, int position){
+
+        }
+
+        @Override
+        public int getItemCount(){
+            return 0;
+        }
+    }
+}
